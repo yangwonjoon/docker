e68879da673981678a56f23e4d82282b0f685b3c
yang <diddnjswns20@gmail.com>
2025-04-03 01:10:39 +0900

    mysql compose

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'wj',
+      database: 'wj',
+      entities: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
